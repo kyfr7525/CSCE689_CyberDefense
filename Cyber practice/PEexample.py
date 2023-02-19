f6110253bf90a417044bdae0d158d4378c1c6437
kyfr7525 <Kylee.Friederichs@Colorado.EDU>
2023-02-19 13:18:07 -0600

    some updates

--- Cyber practice/PEexample.py
+++ Cyber practice/PEexample.py
@@ -3,6 +3,7 @@
 import sys              # Read argv. You can read from file too.
 import pefile           # lib to parse PE file. Change lib to parse ELF
 from sklearn import svm # Some ML classifier, you can change it
+import os
 
 # Receive [goodware, malware, and unknown file] as argument
 gw_train = sys.argv[1]
@@ -15,11 +16,12 @@ unknown  = sys.argv[3]
 # Study task: Consider multiple files
 
 # pe1=pefile.PE(gw_train) # example of parsing PE file
+# sys.argv[1] is a string
 
-for file in sys.argv[1]:
-    for feature in ???:
-        gw_train[file][feature] = parse()
-
+for file in os.listdir(argv[1]):
+    pe1=pefile.PE(gw_train)
+    pe_gw_imps = len(pe1.DIRECTORY_ENTRY_IMPORT)
+    idx+=1
 
 
 # Number of imports (libraries) as feature
@@ -28,6 +30,13 @@ for file in sys.argv[1]:
 # Study task: Consider multiple features!
 pe_gw_imps = len(pe1.DIRECTORY_ENTRY_IMPORT)
 
+
+
+
+
+# for file in sys.argv[1]:
+#     for feature in ???:
+#         gw_train[file][feature] = parse()
 
 
 
@@ -48,29 +57,48 @@ pe_mw_imps = len(pe2.DIRECTORY_ENTRY_IMPORT)
 
 # Create vectors to be classified
 # Feature in X. Labels in Y
-X = [[pe_gw_imps],[pe_mw_imps]]
-# 0=goodware, 1=malware
-Y = [0,1]
+# X = [[pe_gw_imps],[pe_mw_imps]]
+X = [pe_gw_imps + pe_mw_imps]
 
-# Instantiate a classifier and train it with the vectors
-clf = svm.SVC()
-clf.fit(X, Y)
+# 0=goodware, 1=malware
+# Y = [0,1]
+# Y = [0,0,0,1] # create a loop to make this // based on actual num of gw and mw
+Y = [0] * idx_gw + [1] * idx_mw
+
+
+# # Instantiate a classifier and train it with the vectors
+# clf = svm.SVC()
+# clf.fit(X, Y) # x is all features and Y are all labels
 
 # Now parse the unknown file
 # pe3=pefile.PE(unknown)
 
-for file in sys.argv[3]:
-    for feature in ???:
-        unknown[file][feature] = parse()
+# for file in sys.argv[3]:
+#     for feature in ???:
+#         unknown[file][feature] = parse()
+for file in argv[3]:
+    pe3=pefile.PE(unknown)
+    pe_ukn_imps = len(pe3.DIRECTORY_ENTRY_IMPORT)
+    # ask classifier if unknown is malware or goodware
+    res = int(clf.predict([[pe_ukn_imps]]))
+    # Instead of printing 0 or 1, print name
+    label_map = ["goodware","malware"]
+    print(label_map[res])
 
+# pe_ukn_imps = len(pe3.DIRECTORY_ENTRY_IMPORT)
 
-
-
-pe_ukn_imps = len(pe3.DIRECTORY_ENTRY_IMPORT)
-
-# Ask classifier if unknown is malware or goodware
-res = int(clf.predict([[pe_ukn_imps]]))
+# # Ask classifier if unknown is malware or goodware
+# res = int(clf.predict([[pe_ukn_imps]]))
 
 # Instead of printing 0 or 1, print name
-label_map = ["goodware","malware"]
-print(label_map[res])
+# label_map = ["goodware","malware"]
+# print(label_map[res])
+
+
+
+# Instantiate a classifier and train it with the vectors
+clf = svm.SVC()
+clf.fit(X, Y) # x is all features and Y are all labels
+pickle.dump(clf, myfile)
+
+# run file `python filename.py mw/file1.exe gw/file2.exe unknown/file3.exe`
